955f20b19d4990e0d0a6139053ed44609142c62b
Hitansh-Patel656 <hitansh656@gmail.com>
2026-09-26 01:10:59 +0530

    Add food categories and food items

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from app.db.database import Base
 from app.db.database import settings
-from app.models.user import User
+import app.models
 from alembic import context
 
 # this is the Alembic Config object, which provides
